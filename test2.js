1e2e798d22587b024908975463f55fe0aa18d8a8
정 선 미 <kmsun0720@naver.com>
2021-01-01 20:50:15 +0900

    Add : test2.js

---
+++ test2.js
@@ -0,0 +1,16 @@
+function findeven() {
+  let arr = [];
+  
+  for (let i = 0; i <= 50; i++) {
+      if ((i%2) === 0) {
+        arr.push(i)
+      } else if ((i%2) !== 0) {
+        continue
+      }
+  }
+  
+  return arr;
+}
+
+
+findeven()
